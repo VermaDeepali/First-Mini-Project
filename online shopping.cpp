4ccea84b3257e58bf971f34ac0507f04f8a367dd
Deepali Verma <41052882+VermaDeepali@users.noreply.github.com>
2019-03-27 06:33:39 -0700

    Add files via upload

---
+++ online shopping.cpp
@@ -0,0 +1,31 @@
+//programmer Miss Deepali Verma
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+	int item,quan[20],price[30],i;
+	char name[20][20];
+	cout<<"no of item purchased:";
+	cin>>item;
+	for(i=0;i<item;i++)
+	{
+	cout<<"name of item purchased:";
+	cin>>name[i];
+	cout<<"quantity of item purchased:";
+	cin>>quan[i];
+	cout<<"price of items purchased:";
+	cin>>price[i];
+    }
+    system("CLS"); 
+    //CLS is used to clear the input and show only the required output on the scren
+    system("color B0");
+    //color B0 is used to give a particular background color for the output screen
+    cout<<"************Total Bill************"<<endl;
+    cout<<"Name of item\tquantity\tprice\ttotal\t\n";
+    for(i=0;i<item;i++)
+    {
+    	cout<<name[i]<<" \t         "<<quan[i]<<"      \t"<<price[i]<<"      \t"<<quan[i]*price[i]<<"   \n"<<endl;
+	}
+	return 0;
+}
